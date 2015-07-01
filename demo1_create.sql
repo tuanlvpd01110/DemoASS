58239adb4ee622a94c38bf6012c6f6bb0c7b9217
tuanlvpd01110 <tuanlvpd01110@fpt.edu.vn>
2015-07-01 08:28:28 +0700

    tuan
    tuan

---
+++ demo1_create.sql
@@ -0,0 +1,31 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2015-07-01 01:24:51.276
+
+
+
+
+-- tables
+-- Table TNhanvien
+CREATE TABLE TNhanvien (
+    MaVN int    NOT NULL ,
+    TenNV int    NOT NULL ,
+    NgaySinh int    NOT NULL ,
+    Giotinh int    NOT NULL ,
+    Email int    NOT NULL ,
+    MucLuong int    NOT NULL ,
+    MaPhong int    NOT NULL 
+);
+
+-- Table TPhongban
+CREATE TABLE TPhongban (
+    Maphong int    NOT NULL ,
+    Tenphong int    NOT NULL 
+);
+
+
+
+
+
+
+-- End of file.
+
